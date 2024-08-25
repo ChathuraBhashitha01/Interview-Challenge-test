d5cb2561f219e1d165361c1b6ebb72a7ae629231
MSI <chathurabhashitha01@gmail.com>
2024-08-25 09:28:52 +0530

    user entity creation

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,34 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class User {
+
+    @Id
+    private String uid;
+    private String email;
+    private String given_name;
+    private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
+    private String phone_number;
+    private String comment;
+    private String picture;
+    private String directory;
+    private Object metadata;
+    private String tags;
+    private boolean is_suspended;
+    private boolean is_bot;
+}
