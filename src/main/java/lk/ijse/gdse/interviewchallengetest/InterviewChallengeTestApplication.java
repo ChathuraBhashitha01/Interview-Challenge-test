90451f840af64300eadf167c8a6d2ca6b82db692
MSI <chathurabhashitha01@gmail.com>
2024-08-25 09:03:37 +0530

    controller Classes creation

---
+++ src/main/java/lk/ijse/gdse/interviewchallengetest/InterviewChallengeTestApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.interviewchallengetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewChallengeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewChallengeTestApplication.class, args);
+    }
+
+}
